a8a8c0ee1b6df7faea69f16ce6be82d2793dbf28
patriciojavier16 <patriciomoralespaucar@gmail.com>
2023-05-11 17:43:48 -0500

    creacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OfertasComponent } from './components/ofertas/ofertas.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OfertasComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
