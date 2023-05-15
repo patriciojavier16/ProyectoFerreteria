ea668d0e0b78eab4499961798573577c8cd86911
patriciojavier16 <patriciomoralespaucar@gmail.com>
2023-05-15 08:21:00 -0500

    commit-nosotros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OfertasComponent } from './components/ofertas/ofertas.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OfertasComponent,
-    FooterComponent
+    FooterComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule,
